745b271415e3c4447da242b7a0d976bbc80a8eca
aakashvanmali45 <142925372+aakashvanmali45@users.noreply.github.com>
2023-12-07 19:58:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import axios from 'axios';
 const App = () => {
   const [prompt, setPrompt] = useState('');
   const [numImages, setNumImages] = useState(1);
-  const [imageUrl, setImageUrl] = useState('');
+  const [imageData, setImageData] = useState('');
 
   const handleGenerateImage = async () => {
     try {
-      const response = await axios.post('http://localhost:8000/generate_image', {
-        prompt,
+      const response = await axios.post('https://pd-aakas-image-gene-d798d0ab2ce34621ab53775282a56c4f.community.saturnenterprise.io/generate_image', {
+        prompt: prompt,
         num_images: numImages,
       });
-
-      setImageUrl(response.data.image_url);
+  
+      setImageData(response.data.image_data);
     } catch (error) {
-      console.error('Error generating image:', error);
+      if (error.response) {
+        // The request was made, but the server responded with a status code that falls out of the range of 2xx
+        console.error('Error generating image. Server responded with:', error.response.data);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error('Error generating image. No response received from the server.');
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error('Error generating image. Request setup error:', error.message);
+      }
     }
   };
 
@@ -32,10 +41,10 @@ const App = () => {
         <input type="number" value={numImages} onChange={(e) => setNumImages(e.target.value)} />
       </div>
       <button onClick={handleGenerateImage}>Generate Image</button>
-      {imageUrl && (
+      {imageData && (
         <div>
           <h2>Generated Image</h2>
-          <img src={imageUrl} alt="Generated" style={{ maxWidth: '100%' }} />
+          <img src={`data:image/jpeg;base64,${imageData}`} alt="Generated" style={{ maxWidth: '100%' }} />
         </div>
       )}
     </div>
